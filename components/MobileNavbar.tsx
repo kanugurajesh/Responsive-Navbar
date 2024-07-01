51aee54c391166d820b9a1b84cc0f4e83000d541
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-02 01:15:02 +0530

    Responsive navbar is complete

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -6,7 +6,7 @@ import { useState } from 'react'
 const MobileNavbar = () => {
   const [activeMenu, setActiveMenu] = useState(false)
   return (
-    <div>
+    <div className='relative'>
       <nav className="p-4 px-10 flex items-center justify-between">
         <Link
           href="/"
@@ -21,6 +21,40 @@ const MobileNavbar = () => {
           <div className={`w-8 h-1 bg-black origin-[33%_80%] ${activeMenu ? '-rotate-[45deg]' : ''} transition-all ease-in-out duration-300`}></div>
         </div>
       </nav>
+      <div className={`absolute h-screen w-[250px] bg-yellow-500 top-0 ${activeMenu ? 'left-0': 'left-[-300px]'} transition-all ease-in-out duration-300 flex items-center justify-center font-black text-lg text-white`}>
+        <div className="flex flex-col gap-10 p-10">
+          <Link
+            href="/"
+            className="hover:text-black transition-all ease-in-out duration-300"
+          >
+            Home
+          </Link>
+          <Link
+            href="/"
+            className="hover:text-black transition-all ease-in-out duration-300"
+          >
+            About
+          </Link>
+          <Link
+            href="/"
+            className="hover:text-black transition-all ease-in-out duration-300"
+          >
+            Services
+          </Link>
+          <Link
+            href="/"
+            className="hover:text-black transition-all ease-in-out duration-300"
+          >
+            Login
+          </Link>
+          <Link
+            href="/"
+            className="hover:text-black transition-all ease-in-out duration-300"
+          >
+            Contact Us
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
