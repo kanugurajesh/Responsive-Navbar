5031ab7128d1499fbdfe830601e737f8ccdfa393
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-02 00:35:02 +0530

    Adding new ui components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,14 +12,14 @@ const Navbar = () => {
         <h1 className=''>moTrack</h1>
       </Link>
       <div className='flex gap-10 font-bold relative'>
-        <Link href="/" onClick={() => setActiveTab(1)} className='hover:text-yellow-500 transition-all ease-in-out duration-300'>Home</Link>
-        <Link href="/" onClick={() => setActiveTab(2)} className='hover:text-yellow-500 transition-all ease-in-out duration-300'>About</Link>
-        <Link href="/" onClick={() => setActiveTab(3)} className='hover:text-yellow-500 transition-all ease-in-out duration-300'>Services</Link>
+        <Link href="/" onClick={() => setActiveTab(1)} className='hover:text-yellow-500 transition-all ease-in-out duration-300 hover:scale-105'>Home</Link>
+        <Link href="/" onClick={() => setActiveTab(2)} className='hover:text-yellow-500 transition-all ease-in-out duration-300 hover:scale-105'>About</Link>
+        <Link href="/" onClick={() => setActiveTab(3)} className='hover:text-yellow-500 transition-all ease-in-out duration-300 hover:scale-105'>Services</Link>
         <div className={`absolute w-12 h-1 bg-yellow-500 top-6 transition-all ease-in-out duration-300 ${activeTab == 1 && 'left-0' || activeTab == 2 && 'left-[87px]' || activeTab == 3 && 'left-[185px]'}`}></div>
       </div>
       <div className='flex gap-10 items-center font-bold'>
-        <div className='relative'>
-          <Link href="/login" className='hover:text-yellow-500 transition-all ease-in-out duration-300' onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>Login</Link>
+        <div className='relative hover:scale-105'>
+          <Link href="/" className='hover:text-yellow-500 transition-all ease-in-out duration-300 ' onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>Login</Link>
           <div className={`absolute w-12 h-1 ${hover ? 'bg-yellow-500 left-0': 'bg-white left-[-30px]'} top-6 transition-all ease-in-out duration-300 `}></div>
         </div>
         <Link href="/" className='p-3 bg-yellow-500 rounded-md hover:text-yellow-500 text-white hover:bg-white border-2 border-yellow-500 transition-all ease-in-out duration-300'>Contact Me</Link>
